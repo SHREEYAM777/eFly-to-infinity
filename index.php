a58e1e86c3aa807dce70e757c32217150a155ca6
SHREEYAM <89294226+SHREEYAM777@users.noreply.github.com>
2022-04-14 20:40:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!--font awesome-->
+    <link rel="stylesheet" href="css/all.min.css">
+    <!--google font-->
+    <link href="<link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+    <!--custom css-->
+    <link rel="stylesheet" href="css/style.css">
+    <title>efly to Infinity</title>
+</head>
+<body>
+<!-- Start navigation-->
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark pl-2 fixed-top">
+ <!-- <div class="container-fluid">   this was giving space after guide to code -->
+    <a class="navbar-brand" href="index.php">efly to Infinity</a>
+    <span class="navbar-text">Guide to code!</span>  <!--slogan-->
+   <!-- <marquee class"navbar-text">Shree108</marquee> -->
+    <button class="navbar-toggler" type="button" 
+    data-toggle="collapse" data-target="#navbarNavAltMarkup" 
+    aria-controls="navbarNavAltMarkup" aria-expanded="false"
+     aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <ul class="navbar-nav custom-nav pl-2">
+        <li class="nav-item custom-nav-item"><a href="index.php"
+        class="nav-link">Home</a></li>
+        <li class="nav-item custom-nav-item"><a href="#"
+        class="nav-link">Courses</a></li>
+        <li class="nav-item custom-nav-item"><a href="#"
+        class="nav-link">Payment</a></li>
+        <li class="nav-item custom-nav-item"><a href="#"
+        class="nav-link">My profile</a></li>
+        <li class="nav-item custom-nav-item"><a href="#"
+        class="nav-link">Login</a></li>
+        <li class="nav-item custom-nav-item"><a href="#"
+        class="nav-link">log out</a></li>
+        <li class="nav-item custom-nav-item"><a href="#"
+        class="nav-link">Sign up</a></li>
+        <li class="nav-item custom-nav-item"><a href="#"
+        class="nav-link">Feedback</a></li>
+        <li class="nav-item custom-nav-item"><a href="#"
+        class="nav-link">Contact</a></li>
+      </ul>
+    </div>
+</div>
+</nav>
+
+<!-- End navigation-->
+
+<!--start video backgroung-->
+<div class="container-fluid remove-vid-marg">
+  <div class="vid-parent">
+  
+    <video playsinline autoplay muted Loop>
+      <source src="video/videobanner2.mp4">    
+     <!-- <source src="video/Nature.mp4">  -->
+    </video>
+    <div class="vid-overlay"></div> 
+    <div class="vid-content">
+      <h3 class="my-content">efly to Infinity</h3> 
+      <small class="my-content">Guide to Code</small><br>
+      <a href="#" class="btn btn-danger">Get Started</a> 
+</div>  
+</div>
+
+<!--End video-->
+
+
+
+
+
+
+
+<!-- jquery bootstrap java script--> 
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script scr="js/bootstrap.min.js"></script>
+<!--font awesome-->
+<script src="js/all.min.js"></script>
+</body>
+</html>
